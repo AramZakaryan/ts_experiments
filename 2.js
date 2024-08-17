40e61ae3e679243d3fe515686d62a27960f99f84
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-08-17 12:54:03 +0400

    current Proxy indexed

--- 2.js
+++ 2.js
@@ -1,43 +1,41 @@
-// const position = {
-//     x: 24, y: 45, _direction: "north"
-//
-// }
-//
-// const positionProxy = new Proxy(position, {
-//     get(target, key) {
-//         return key.startsWith('_') ? 'hidden' : target[key]
-//     }
-// })
-//
-// console.log(positionProxy.x, // Output: 24
-//     positionProxy.y, // Output: 45
-//     positionProxy._direction // Output: 'hidden'
-// )
+const people = [
+    {id: 'id001', name: 'Alice', job: 'Engineer', age: 30},
+    {id: 'id002', name: 'Bob', job: 'Designer', age: 25},
+    {id: 'id003', name: 'Charlie', job: 'Teacher', age: 35},
+    {id: 'id004', name: 'Diana', job: 'Doctor', age: 40},
+    {id: 'id005', name: 'Eve', job: 'Artist', age: 28}
+]
 
+let ArrayIndexed = new Proxy(Array, {
+    construct(target, [arg]) {
+        const arrIndexedById = {}
+        arg.forEach(a => arrIndexedById[a.id] = a)
+        return new Proxy(new target(...arg), {
+            get(target, prop) {
+                switch (prop) {
+                    case 'push':
+                        return item => {
+                            arrIndexedById[item.id] = item
+                            target[prop].call(target, item)
+                        }
+                    case 'findById':
+                        return id => arrIndexedById[id]
+                    default:
+                        return target[prop]
+                }
 
-// console.log(
-//     "Hello!".startsWith('H') // Output: true
-// )
-
-
-const person = {
-    name: "Gago",
-    age: 56,
-    job: "fullstack"
-}
-
-const personProxy = new Proxy(person, {
-    has: (target, key) => Object.prototype.hasOwnProperty.call(target, key),
-    ownKeys(target, keys) {
-        return Object.getOwnPropertyNames(target)
+            }
+        })
     }
 })
 
+const peopleProxy = new ArrayIndexed(people)
+
+
+peopleProxy.push({id: 'id006', name: 'Lil', job: 'cook', age: 48})
+
 console.log(
-    Object.keys(personProxy) // ['name', 'age', 'job']
+    peopleProxy[5], // {id: 'id006', name: 'Lil', job: 'cook', age: 48}
+    peopleProxy.findById('id006') // {id: 'id006', name: 'Lil', job: 'cook', age: 48}
 )
 
-
-let x = void 0
-
-console.log(x) // undefined
