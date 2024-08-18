1ee30c7752d4da4ea5c5e75c0f3e5d2ef9e20b02
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-08-18 13:40:10 +0400

    current XHR

--- 2.js
+++ 2.js
@@ -1,12 +1,108 @@
-function* func() {
-    yield 'H';
-    yield 'e';
-    yield 'l';
-    yield 'l';
-    yield 'o';
-    yield '!';
+// // const personMap = new Map([
+// //     ['name', 'Gago'],
+// //     ['age', 56],
+// //     [50, true],
+// //     [{address: "Yerevan"}, true],
+// //     [NaN, 'empty']
+// // ])
+// //
+// // console.log(
+// //     Object.fromEntries(personMap)   // {
+// //                                     // 50: true,
+// //                                     // name: 'Gago',
+// //                                     // age: 56,
+// //                                     // [object Object]: true,
+// //                                     // NaN: 'empty'
+// //                                     // }
+// // )
+// //
+// // const object = {'name': 'John'};
+// //
+// // console.log(
+// //     object.toString() // [object Object]
+// // )
+// // ;
+//
+//
+// const f = n => n % 2 ? 'Odd' : 'Even';
+//
+// const evenOrOdd = new Proxy(
+//     function (n) {
+//         return f(n);
+//     },
+//     {
+//         get: (_, prop) => new Proxy(function () {
+//             return f(prop);
+//         }, {
+//             get: target => 'xuy',
+//             construct: _ => new String(f(prop))
+//         }),
+//         construct: (_, [n]) => new String(f(n)),
+//     });
+//
+// console.log(
+//     // evenOrOdd(2), //'Even'
+//     evenOrOdd[2], //'Even'
+//     // new evenOrOdd(2), //'Even'
+//     // new evenOrOdd[2], //'Even'
+//     // evenOrOdd(7), //'Odd'
+//     evenOrOdd[7], //'Odd'
+//     // new evenOrOdd(7), //'Odd'
+//     // new evenOrOdd[7], //'Odd'
+// )
+
+
+const xml = new XMLHttpRequest()
+
+xml.open('POST', 'https://jsonplaceholder.typicode.com/posts', true);
+
+xml.responseType = 'json';
+
+xml.setRequestHeader('Content-type', 'application/json');
+
+xml.onload = () => {
+    if (xml.status >= 400) {
+        console.error(xml.response);
+    }
+    console.log(xml.response);
+
 }
 
-const gen = func()
+xml.onerror = () => {
+    console.log(xml.response)
+}
 
-console.log(gen.next());
+const body = {
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+}
+
+xml.send(JSON.stringify(body));
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
